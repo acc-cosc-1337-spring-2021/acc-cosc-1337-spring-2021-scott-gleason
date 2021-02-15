07b130fbdcada524d2645ee5252fa4da58f5b54e
scott-gleason <scott.gleason@g.austincc.edu>
2021-02-14 18:53:05 -0600

    homework 2

--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -1,5 +1,7 @@
 #include "variables.h"
 
+const double tax_rate = .0675;
+
 //example
 int add_numbers(int num1, int num2)
 {
@@ -8,6 +10,16 @@ int add_numbers(int num1, int num2)
 
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
+
+double get_sales_tax(double meal_amount)
+{
+	return meal_amount * tax_rate;
+}
+
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
 
 
 
--- test/homework_test/01_variables_test/01_variables_tests.cpp
+++ test/homework_test/01_variables_test/01_variables_tests.cpp
@@ -12,3 +12,19 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Test Get Sales Tax", "Verify function returns .675 using parameter of 10") {
+	REQUIRE(get_sales_tax(10) == .675);
+}
+
+TEST_CASE("Test Get Sales Tax 1", "Verify function returns .1.35 using parameter of 20") {
+	REQUIRE(get_sales_tax(20) == 1.35);
+}
+
+TEST_CASE("Test Get Tip Amount 1", "Verify function returns 3 using parameter of 20 and .15") {
+	REQUIRE(get_tip_amount(20, .15) == 3);
+}
+
+TEST_CASE("Test Get Tip Amount 2", "Verify function returns 4 using parameter of 20 and .20") {
+	REQUIRE(get_tip_amount(20, .20) == 4);
+}
+
