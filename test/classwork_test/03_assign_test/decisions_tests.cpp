f44a722d33037c23e47e46ef44d6b28ab950d6a3
scott-gleason <scott.gleason@g.austincc.edu>
2021-02-16 21:17:24 -0600

    Homework 3

--- test/classwork_test/03_assign_test/decisions_tests.cpp
+++ test/classwork_test/03_assign_test/decisions_tests.cpp
@@ -1,8 +1,27 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include "decision.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify Get Letter Grade Using If", "Verify grade returns correct letter grade") 
+{
+	REQUIRE(get_letter_grade_using_if(95) == "A");
+	REQUIRE(get_letter_grade_using_if(85) == "B");
+	REQUIRE(get_letter_grade_using_if(75) == "C");
+	REQUIRE(get_letter_grade_using_if(65) == "D");
+	REQUIRE(get_letter_grade_using_if(55) == "F");
+}
+
+TEST_CASE("Verify Get Letter Grade Using Switch", "Verify grade returns correct letter grade.")
+{
+	REQUIRE(get_letter_grade_using_switch(95) == "A");
+	REQUIRE(get_letter_grade_using_switch(85) == "B");
+	REQUIRE(get_letter_grade_using_switch(75) == "C");
+	REQUIRE(get_letter_grade_using_switch(65) == "D");
+	REQUIRE(get_letter_grade_using_switch(55) == "F");
 }
 
